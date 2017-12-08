0f499de44dff6cd317c114be664dce30e5817735
Omer Celik <omerfcelik92@gmail.com>
2017-12-08 15:53:48 +0300

    Example 3.2 with mock response/request

---
+++ service-workers/01/032/service-worker.js
@@ -0,0 +1,37 @@
+function log(msg) {
+
+    console.log(msg);
+
+}
+
+self.addEventListener('fetch', function(){
+
+    log('serviceWorker is currently handling fetch event for ' + evet.request.url);
+
+    if( requestUrl.pathname === '/urlshortener/v1/url' && event.request.headers.has('X-Mock-Response') ) {
+
+        var response = {
+            body : {
+                kind : 'urlshortener#url',
+                id : 'http://goo.gl/IKyjuU',
+                longUrl : 'https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html'
+            },
+            init : {
+                status : 200,
+                statusText : 'OK',
+                headers : {
+                    'Content-Type': 'application/json',
+                    'X-Mock-Response' : 'yes'
+                }
+            }
+        };
+
+        var mockResponse = new Response(JSON.stringify(response.body), response.init);
+
+        log("Responding with a mock response body: " + response.body);
+
+        event.respondWith(mockResponse);
+
+    }
+
+});
