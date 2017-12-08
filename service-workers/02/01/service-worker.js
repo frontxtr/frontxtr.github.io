0946071c31d780045fedf2264ead718bf6407ed2
Omer Celik <omerfcelik92@gmail.com>
2017-12-08 18:02:00 +0300

    js changes

--- service-workers/02/01/service-worker.js
+++ service-workers/02/01/service-worker.js
@@ -1,51 +1,49 @@
 'use strict';
-
 var version = 1;
-
 var currentCache = {
     offline: 'offline-cache' + version
 };
-
-var offlineURL = 'offline.html';
-
-self.addEventListener('install', function(event){
-    event.waitUntil(caches.open(currentCache.offline).then(function(cache){
-        return cache.addAll([
-            offlineURL
-        ]);
-    }));
+var offlineUrl = 'offline.html';
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(currentCache.offline).then(function(cache) {
+            return cache.addAll([
+                offlineUrl
+            ]);
+        })
+    );
 });
-
-self.addEventListener('fetch', function(event){
-    var request = event.request, isRequestMethodGET = request.method === 'GET';
-
-    if( request.mode === 'navigate' || isRequestMethodGET ) {
-
-        event.respondWith(fetch.createRequestWithCacheBusting(request.url)).catch(function(error){
-            console.log("OFFLINE: returning offline page", error);
-            return caches.match(offlineURL);
-        });
-
+self.addEventListener('fetch', function(event) {
+    var request = event.request,
+        isRequestMethodGET = request.method === 'GET';
+    if (request.mode === 'navigate' || isRequestMethodGET) {
+        event.respondWith(
+            fetch(createRequestWithCacheBusting(request.url)).catch(function(error) {
+                console.log('OFFLINE: Returning offline page.',
+                    error);
+                return caches.match(offlineUrl);
+            })
+        );
     } else {
-        event.respondWith(caches.match(request).then(function(response){
-            return response || fetch(request);
-        }));
+        event.respondWith(caches.match(request)
+            .then(function(response) {
+                return response || fetch(request);
+            })
+        );
     }
-
 });
 
 function createRequestWithCacheBusting(url) {
-    var request, cacheBustingUrl;
-
-    request = new Request(url, {cache:'reload'})
-
-    if('cache' in request) {
+    var request,
+        cacheBustingUrl;
+    request = new Request(url, {
+        cache: 'reload'
+    });
+    if ('cache' in request) {
         return request;
     }
-
     cacheBustingUrl = new URL(url, self.location.href);
-    cacheBustingUrl.search += (cacheBustingUrl.search ? '&' : '') + 'cachebust=' + Date.now();
-
+    cacheBustingUrl.search += (cacheBustingUrl.search ? '&' :
+        '') + 'cachebust=' + Date.now();
     return new Request(cacheBustingUrl);
-
 }
