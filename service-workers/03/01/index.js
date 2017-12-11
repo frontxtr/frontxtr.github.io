9b2af2dfc45b2dc9d91f52603de13aad76b524c6
Omer Celik <omerfcelik92@gmail.com>
2017-12-11 18:07:41 +0300

    remote game go offline

---
+++ service-workers/03/01/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var scope = {
+    scope : './'
+};
+
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register(
+        'service-worker.js', scope
+    ).then(function(serviceWorker){
+        printStatus("Successfull");
+    }).catch(function(error){
+        printStatus("Error!" + error);
+    });
+
+} else {
+    printStatus("Not available");
+}
+
+navigator.serviceWorker.addEventListener('controllerchange', function(event){
+    console.log("EVENT: controllerchange", event);
+
+    navigator.serviceWorker.controller.addEventListener('statechange', function(){
+
+        if( this.state === "activated" ) {
+            document.querySelector('#notification').classList.remove('hidden';)
+        }
+
+    })
+
+});
+
+function printStatus(status) {
+    document.querySelector('#status').innerHTML = status;
+}
+
+document.querySelector("#resetButton").addEventListener('click',
+    function(){
+    navigator.serviceWorker.getRegistration.then(function(registration){
+        registration.unregister();
+        window.location.reload();
+    })
+    });
---
+++ service-workers/03/01/service-worker.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var cacheName = 'dependices-cache';
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll([
+                'apple',
+                'google',
+                'adobe',
+                'facebook',
+                'amazon'
+            ]);
+        }).then(function(){
+            self.skipWaiting();
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+                if(response) {
+                    console.log("Fetching from cache, ", event.request.url);
+                    return response;
+                } else {
+                    console.log("Fetching from server, ", event.request.url);
+                }
+                return fetch(event.request);
+            })
+    )
+});
+
+self.addEventListener('activate', function(event){
+    event.waitUntil(self.clients.claim());
+});
