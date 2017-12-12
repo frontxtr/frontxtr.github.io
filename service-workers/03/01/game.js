b9b23ee62204064fe67bd76f30feee6f429fdaef
Omer Celik <omerfcelik92@gmail.com>
2017-12-12 15:02:55 +0300

    remote game go offline

--- service-workers/03/01/game.js
+++ service-workers/03/01/game.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var attempts = 0,
     images = [
         'adobe',
@@ -8,22 +7,22 @@ var attempts = 0,
         'facebook',
         'amazon'
     ];
-
-document.getElementById("tryButton").addEventListener('click', function(){
-    var imageElement = document.getElementById("logo"),
-        choice = document.querySelector("#choice").value,
-        attemptsEl = document.querySelector("#attempts"),
-        result = document.querySelector("#result"),
+document.getElementById('tryButton').addEventListener
+('click', function() {
+    var imageElement = document.getElementById('logo'),
+        choice = document.querySelector('#choice').value,
+        attemptsEl = document.querySelector('#attempts'),
+        result = document.querySelector('#result'),
         currentIndex = imageElement.getAttribute('data-image'),
         newIndex = getRandomIndex();
     do {
         newIndex = getRandomIndex();
-    } while (newIndex === currentIndex);
+    } while(newIndex === currentIndex);
     imageElement.src = images[newIndex] + '-logo.png';
     imageElement.setAttribute('data-image', newIndex);
     result.className = '';
     attempts++;
-    if (newIndex == choice) {
+    if(newIndex == choice) {
         result.innerText = "Yay! Well done! You did it in " +
             attempts + " attempt(s)";
         result.classList.add('success');
@@ -35,7 +34,6 @@ document.getElementById("tryButton").addEventListener('click', function(){
         attemptsEl.innerText = attempts;
     }
 });
-
 function getRandomIndex() {
     return Math.floor(Math.random() * 5);
 }
--- service-workers/03/01/service-worker.js
+++ service-workers/03/01/service-worker.js
@@ -1,37 +1,40 @@
 'use strict';
-var cacheName = 'dependices-cache';
-
-self.addEventListener('install', function(event){
+var cacheName= 'dependencies-cache';
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(cacheName).then(function(cache){
-            return cache.addAll([
-                'apple',
-                'google',
-                'adobe',
-                'facebook',
-                'amazon'
-            ]);
-        }).then(function(){
-            self.skipWaiting();
-        })
-    )
+        caches.open(cacheName)
+            .then(function(cache) {
+                return cache.addAll([
+                    'apple',
+                    'google',
+                    'adobe',
+                    'facebook',
+                    'amazon'
+                ]);
+            })
+            .then(function() {
+                return self.skipWaiting();
+            })
+    );
 });
-
-self.addEventListener('fetch', function(event){
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-            .then(function(response){
-                if(response) {
-                    console.log("Fetching from cache, ", event.request.url);
-                    return response;
-                } else {
-                    console.log("Fetching from server, ", event.request.url);
+            .then(function(response) {
+                    if (response) {
+                        console.log('Fetching from the cache: ',
+                            event.request.url);
+                        return response;
+                    } else {
+                        console.log('Fetching from server: ',
+                            event.request.url);
+                    }
+                    return fetch(event.request);
                 }
-                return fetch(event.request);
-            })
-    )
+            )
+    );
 });
-
-self.addEventListener('activate', function(event){
+self.addEventListener('activate', function(event) {
+    console.log('Activating the service worker!');
     event.waitUntil(self.clients.claim());
 });
