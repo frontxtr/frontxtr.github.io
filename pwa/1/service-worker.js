253c19b36f9914efbff92631b5239d59be4cd085
Omer Celik <omerfcelik92@gmail.com>
2017-12-13 15:02:16 +0300

    cache olani veya normal olanı getir

--- pwa/1/service-worker.js
+++ pwa/1/service-worker.js
@@ -13,7 +13,13 @@ self.addEventListener('install', function(event){
 self.addEventListener('fetch', function(event){
     event.respondWith(
         caches.match(event.request).then(function(response){
-            return response || fetch(event.request);
+            return response || fetch(event.request).then(function(response){
+                console.log("fetched from network this time!");
+                return caches.open('version1').then(function(cache){
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+            })
         })
     );
 });
