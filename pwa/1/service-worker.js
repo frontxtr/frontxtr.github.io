f4f985be33dff1ada59728fa7fce9b2d333a2ded
Omer Celik <omerfcelik92@gmail.com>
2017-12-13 14:48:57 +0300

    another offline example

--- pwa/1/service-worker.js
+++ pwa/1/service-worker.js
@@ -1,3 +1,15 @@
+self.addEventListener('install', function(event){
+   if(!('caches' in self)) return;
+   event.waitUntil(
+       caches.open('version1').then(function(cache){
+           return cache.addAll([
+               'index.html',
+               'images/i-love-pirates.jpg'
+           ])
+       })
+   )
+});
+
 self.addEventListener('fetch', function(event){
     event.respondWith(new Response('fuck offf'));
 });
