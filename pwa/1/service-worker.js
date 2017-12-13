4ab5a711e6f6ddbff7f69e98f0bd84e608a430a3
Omer Celik <omerfcelik92@gmail.com>
2017-12-13 14:51:13 +0300

    another offline example

--- pwa/1/service-worker.js
+++ pwa/1/service-worker.js
@@ -11,5 +11,5 @@ self.addEventListener('install', function(event){
 });
 
 self.addEventListener('fetch', function(event){
-    event.respondWith(new Response('fuck offf'));
+    event.respondWith(fetch(event.request));
 });
