bf062d5084c0740879a5f367575d26cf6f960f16
Omer Celik <omerfcelik92@gmail.com>
2017-12-13 15:21:54 +0300

    offline sayfa denemesi

--- pwa/2/script.js
+++ pwa/2/script.js
@@ -1,6 +1,6 @@
 if( 'serviceWorker' in navigator ) {
 	window.addEventListener('load', function(){
-		navigator.serviceWorker.register('service-worker')
+		navigator.serviceWorker.register('service-worker.js')
 			.then(function(reg){
 				console.log("ServiceWorker Registration completed", reg);
 			})
