cfd10d3862125a1d2fff1bdc1dd9fcb6f4b6e821
Omer Celik <omerfcelik92@gmail.com>
2017-12-14 14:06:32 +0300

    sw-toolbox sample

---
+++ pwa/5/service-worker.js
@@ -0,0 +1,21 @@
+importScripts('sw-toolbox/sw-toolbox.js');
+
+self.addEventListener('install', function(event){
+    // nothing to do here
+});
+
+toolbox.router.get('/images/*', toolbox.fastest, {
+    cache : {
+        name: 'sw-toolbox-version1',
+        maxEntries : 20,
+        maxAgeSeconds : 60 * 30
+    }
+});
+
+toolbox.router.get('/styles/*', toolbox.cacheFirst, {
+    cache : {
+        name: 'sw-toolbox-version1',
+        maxEntries : 20,
+        maxAgeSeconds : 60 * 60 * 24 * 7
+    }
+});
