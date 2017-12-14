a2fd01fc597bcfe78afb0292da6bf04148ca6451
Omer Celik <omerfcelik92@gmail.com>
2017-12-14 14:43:59 +0300

    sw-toolbox sample

--- pwa/5/service-worker.js
+++ pwa/5/service-worker.js
@@ -1,13 +1,38 @@
 importScripts('sw-toolbox.js');
 
-self.addEventListener('install', function(event){
-    // nothing to do here
+self.addEventListener('install', (event) => {
+
 });
 
 toolbox.router.get('/images/*', toolbox.fastest, {
-    cache : {
+    cache: {
         name: 'sw-toolbox-version1',
-        maxEntries : 20,
-        maxAgeSeconds : 60 * 30
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 30
+    }
+});
+
+toolbox.router.get('/styles/*', toolbox.cacheFirst, {
+    cache: {
+        name: 'sw-toolbox-version1',
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 60 * 24 * 7
+    }
+});
+
+toolbox.router.get('*.html', toolbox.cacheFirst, {
+    cache: {
+        name: 'sw-toolbox-version1',
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 60 * 24 * 7
+    }
+});
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+    origin: 'openlibrary.org',
+    cache: {
+        name: 'sw-toolbox-version1',
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 60 * 12
     }
 });
